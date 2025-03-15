fe83e73c198344e1f9114f7929bdaee5740c6a9a
Alsaef <saef.ratul@gmail.com>
2025-03-15 11:07:17 +0600

    style change

--- scripts/api.js
+++ scripts/api.js
@@ -152,8 +152,8 @@ const showSingleWordData=(word)=>{
     const modal=document.getElementById('my_modal_5')
       showWordData.innerHTML=`
         <div>
-            <h2 class="text-xl font-bold">
-              ${word.word} <span class="text-gray-500">(উঃ ${word.pronunciation})</span>
+             <h2 class="text-xl font-bold flex gap-2 items-center">
+              ${word.word} <span class="text-black flex items-center gap-3">(<img src='../assets/fi-ss-microphone.png' />  :${word.pronunciation} )</span>
             </h2>
             <div class="mt-2">
               <p class="font-semibold">Meaning</p>
